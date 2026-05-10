857dec011c848eaf9f8c069a7d3d43ae4bf8d0e9
Zanubiya <zanurauf@gmail.com>
2026-05-10 21:20:22 +0300

    #2nd changes made server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
-
 const express = require("express");
-
 const cors = require("cors");
-
 const dotenv = require("dotenv");
-
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -14,17 +10,19 @@ connectDB();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-
 app.use("/api/bookings", require("./routes/bookingRoutes"));
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server Running");
+// PORT FIX (IMPORTANT)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
 });
