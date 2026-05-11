15194581a6c6d00602d9693f1f843393f337b454
Zanubiya <zanurauf@gmail.com>
2026-05-11 03:23:25 +0300

    bootsrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // Remove this line if not installed:
 // import "bootstrap-icons/font/bootstrap-icons.css";
 
